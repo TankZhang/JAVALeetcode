ee483fa3dcb69354b9bd4b87dcc6d3007280fedb
Tank Zhang <tankzhang9712@outlook.com>
2016-09-15 12:06:32 +0800

    添加第八题

--- src/com/JAVALeetcode/Main.java
+++ src/com/JAVALeetcode/Main.java
@@ -6,65 +6,114 @@ import java.lang.reflect.Array;
 import java.util.*;
 import java.util.regex.Pattern;
 
+//region 初始化块的学习，静态初始化块的学习
+/*
+class Root {
+    static {
+        System.out.println("root的静态初始化块");
+    }
+
+    {
+        System.out.println("Root的普通初始化块");
+    }
+
+    public Root() {
+        System.out.println("Root的无参数的构造器");
+    }
+}
+
+class Mid extends Root {
+    static {
+        System.out.println("Mid的静态初始化块");
+    }
+    {
+        System.out.println("Mid的普通初始化块");
+    }
+    public  Mid(){
+        System.out.println("Mid的无参数的构造器");
+    }
+    public  Mid(String msg){
+        this();
+        System.out.println("Mid的带参数的构造器，参数为："+msg);
+    }
+}
+
+class Leaf extends  Mid{
+    static  {
+        System.out.println("Leaf的静态初始化块");
+    }
+    {
+        System.out.println("Leaf的普通初始化块");
+    }
+    public  Leaf(){
+        super("JAVA");
+        System.out.println("执行leaf的构造器");
+    }
+}
+*/
+//endregion
 
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(myAtoi(str));
+    }
+
+
+    //region 8. String to Integer (atoi)
+    //https://leetcode.com/problems/string-to-integer-atoi/
+    static int myAtoi(String str) {
+        while(str.startsWith(" "))
+            str = str.substring(1);
+        boolean flag=true;
+        long res=0;
+        if(str.startsWith("+"))
+            str=str.substring(1);
+        else if(str.startsWith("-")){
+            str=str.substring(1);
+            flag=false;
+        }
+        for (int i=0;i<str.length();i++)
+        {
+            if(Character.isDigit(str.charAt(i))){
+                res=res*10+Integer.parseInt(String.valueOf(str.charAt(i)));
+                if(res>Integer.MAX_VALUE)
+                {
+                    if(flag)
+                        return  2147483647;
+                    else
+                        return  -2147483648;
+                }
+            }
+            else
+                break;
+        }
+        return  (int)res*(flag?1:-1);
 
     }
 
 
-    static void  Test8_2_1()
-    {
-        Collection book=new HashSet();
+    // endregion
+
+    static void Test8_2_1() {
+        Collection book = new HashSet();
         book.add("a");
         book.add("b");
         book.add("c");
-        Iterator it=book.iterator();
-        while (it.hasNext())
-        {
-            String b=(String)it.next();
+        Iterator it = book.iterator();
+        while (it.hasNext()) {
+            String b = (String) it.next();
             System.out.println(b);
-            if(b.equals("b"))
+            if (b.equals("b"))
                 it.remove();
         }
         System.out.println(book);
     }
 
-/*
-class  A{
-    public  static  int a=print(1);
-    public  int aa=print(2);
-    static  {print(3);}
-    {print(4);}
-    public  A(String s)
-    {
-        System.out.println(5);
-    }
-    public  static  int print(int i)
-    {
-        System.out.println(i);
-        return  i;
-    }
-}
-
-class  B extends  A{
-    public  static  int b=print(6);
-    public  int bb=print(7);
-    static  {print(8);}
-    public  B(String s){
-        super(s);
-        System.out.println(s);
-    }
-    public  static  int print(int i){
-        System.out.println(i);
-        return  i;
-    }
-}
-
-*/
 
     /* //房子数目
 
@@ -128,24 +177,21 @@ class  B extends  A{
         }
     */
     //反转数组
-    static  void  reverse(int[] arr)
-    {
-        for (int i=0;i<arr.length/2;i++)
-        {
-            int temp=arr[i];
-            arr[i]=arr[arr.length-1-i];
-            arr[arr.length-1-i]=temp;
+    static void reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
         }
     }
 
     //正则表达式
-    static  void  Zhenzebiaodas()
-    {
-        String regex1="[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+";
-        String value="192.168.1.1";
-        String regex2="[0-9]*\\.[0-9]*\\.[0-9]*\\.[0-9]*";
-        String regex3="[0-9]{1,}\\.[0-9]{1,}\\.[0-9]{1,}\\.[0-9]{1,}";
-        String regex4="\\d+\\.\\d+\\.\\d+\\.\\d+";
+    static void Zhenzebiaodas() {
+        String regex1 = "[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+";
+        String value = "192.168.1.1";
+        String regex2 = "[0-9]*\\.[0-9]*\\.[0-9]*\\.[0-9]*";
+        String regex3 = "[0-9]{1,}\\.[0-9]{1,}\\.[0-9]{1,}\\.[0-9]{1,}";
+        String regex4 = "\\d+\\.\\d+\\.\\d+\\.\\d+";
         System.out.println(value.matches(regex1));
         System.out.println(value.matches(regex2));
         System.out.println(value.matches(regex3));
@@ -154,17 +200,16 @@ class  B extends  A{
     }
 
     //看想等
-    static void  shifouxiangdeng()
-    {
-        String s1=new   String("abc");
-        String s2="abc";
-        String s3=new String("abc");
-        String s4="a"+"bc";
-        String s5=new String("a")+"bc";
-        System.out.println(s1==s2);
-        System.out.println(s4==s5);
-        System.out.println(s1==s3);
-        System.out.println(s2==s4);
+    static void shifouxiangdeng() {
+        String s1 = new String("abc");
+        String s2 = "abc";
+        String s3 = new String("abc");
+        String s4 = "a" + "bc";
+        String s5 = new String("a") + "bc";
+        System.out.println(s1 == s2);
+        System.out.println(s4 == s5);
+        System.out.println(s1 == s3);
+        System.out.println(s2 == s4);
 
     }
 
@@ -304,50 +349,79 @@ class  B extends  A{
 */
 
 
-
-
-
-
     //输入输出
     private static void Test_in_out() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        String[] ss=input.split(" ");
-        System.out.println(ss[ss.length-1].length());
+        String[] ss = input.split(" ");
+        System.out.println(ss[ss.length - 1].length());
     }
 
     private static void Test7_5_2() {
-        boolean b= Pattern.matches("a.*b","adsdsadab");
+        boolean b = Pattern.matches("a.*b", "adsdsadab");
         System.out.println(b);
     }
 
-    public static  void  Test7_4_2()
-    {
-        Calendar c=Calendar.getInstance();
+    public static void Test7_4_2() {
+        Calendar c = Calendar.getInstance();
         System.out.println(c.get(Calendar.YEAR));
         System.out.println(c.get(Calendar.MONTH));
         System.out.println(c.get(Calendar.DATE));
-        c.set(2016,9,12,9,12,12);
+        c.set(2016, 9, 12, 9, 12, 12);
         System.out.println(c.getTime());
     }
 
-    public static void  Test7_4_1()
-    {
-        Date d1=new Date();
-        Date d2=new Date(System.currentTimeMillis()+100);
+    public static void Test7_4_1() {
+        Date d1 = new Date();
+        Date d2 = new Date(System.currentTimeMillis() + 100);
         System.out.println(d2);
         System.out.println(d1.compareTo(d2));
         System.out.println(d1.before(d2));
     }
 
     //递归求和
-    public static  int get(int n)
-    {
-        if(n==1)
-            return  1;
-        else if(n==2)
-            return  4;
+    public static int get(int n) {
+        if (n == 1)
+            return 1;
+        else if (n == 2)
+            return 4;
         else
-            return  get(n-1)*2+get(n-2);
+            return get(n - 1) * 2 + get(n - 2);
     }
 }
+
+
+//region 静态类学习
+/*
+class  A{
+    public  static  int a=print(1);
+    public  int aa=print(2);
+    static  {print(3);}
+    {print(4);}
+    public  A(String s)
+    {
+        System.out.println(5);
+    }
+    public  static  int print(int i)
+    {
+        System.out.println(i);
+        return  i;
+    }
+}
+
+class  B extends  A{
+    public  static  int b=print(6);
+    public  int bb=print(7);
+    static  {print(8);}
+    public  B(String s){
+        super(s);
+        System.out.println(s);
+    }
+    public  static  int print(int i){
+        System.out.println(i);
+        return  i;
+    }
+}
+
+*/
+//endregion
